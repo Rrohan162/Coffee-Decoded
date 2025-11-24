cc8b01e4228d47d9705a48920314a6f8fbe74d88
Rohan Mayekar <60402307+Rrohan162@users.noreply.github.com>
2025-11-25 03:48:26 +0530

    Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Coffee-Decoded/',   // <-- ADD THIS FOR GITHUB PAGES
+    base: '/Coffee-Decoded/',      // <- set this to "/<your-repo-name>/"
     server: {
       port: 3000,
       host: '0.0.0.0',
